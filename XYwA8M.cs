657c94e05230188ed82cd6b7c5b2d596d2e3720b
ku8y_ <154291835+ku8y@users.noreply.github.com>
2025-12-17 03:11:12 +0900

    Add files via upload

---
+++ XYwA8M.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[CompilerGenerated]
+internal class XYwA8M
+{
+	internal static XYwA8M.SrrFdJVum H5AUtcRehKK;
+
+	internal static int 4aYWn5EWtb;
+
+	internal static XYwA8M.wx0UO PJxINv7h;
+
+	internal static long CkrKdcyqOy;
+
+	internal static XYwA8M.nJ9Y tmAK;
+
+	internal static XYwA8M.nJ9Y xGWXngK24;
+
+	internal static XYwA8M.H2k0 8mRQgMpo;
+
+	internal static XYwA8M.nJ9Y R6drU193;
+
+	internal static XYwA8M.XVp0qCwTGH srw;
+
+	internal static long cQQPWE;
+
+	internal static XYwA8M.XVp0qCwTGH z14EALD3;
+
+	internal static XYwA8M.XVp0qCwTGH R9E;
+
+	internal static XYwA8M.DvHRf2 W2tUZCfnnFK;
+
+	internal static XYwA8M.yOzi39QomFd xmJA;
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 144)]
+	private struct SrrFdJVum
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 24)]
+	private struct wx0UO
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 32)]
+	private struct nJ9Y
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 11)]
+	private struct H2k0
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 16)]
+	private struct XVp0qCwTGH
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 84)]
+	private struct DvHRf2
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 10)]
+	private struct yOzi39QomFd
+	{
+	}
+}
---
+++ j7xqcJiG4H.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public static class j7xqcJiG4H
+{
+    public unsafe static string wUK(int lgFaCzXanNo)
+    {
+        bool isNegative = (lgFaCzXanNo >> 31 != 0);
+        if (isNegative)
+            return string.Empty;
+
+        byte* ptr = (byte*)(&j7xqcJiG4H.HZcFo);
+        ptr += (lgFaCzXanNo ^ *(int*)ptr);
+
+        int length = *(int*)ptr;
+        byte[] buffer = new byte[length];
+
+        fixed (byte* bufferPtr = &buffer[0])
+        {
+            cpblk(bufferPtr, ptr + 8, length);
+        }
+
+        bool isOdd = (buffer.Length % 2 != 0);
+        if (isOdd)
+        {
+            int mid = buffer.Length >> 1;
+            buffer[mid] ^= *(int*)(ptr + 4);
+        }
+
+        int end = buffer.Length - 1;
+        for (int i = 0; i <= end; i++)
+        {
+            buffer[i] ^= (byte)((int)buffer[end] ^ *(int*)(ptr + 4));
+            end--;
+        }
+
+        return string.Intern(Encoding.UTF8.GetString(buffer));
+    }
+
+    private static j7xqcJiG4H.KSi4QsZ HZcFo;
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 19409)]
+	private struct KSi4QsZ
+	{
+	}
+}
